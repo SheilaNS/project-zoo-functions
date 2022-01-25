e55146b48ed124fba918c8f94457463c891cef71
SheilaNS <shei.nsantos@gmail.com>
2022-01-25 15:18:54 -0300

    Requisito 1 ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-  
+function getSpeciesByIds(...ids) {
+  if (ids === undefined) {
+    return [];
+  }
+  const species = ids.map((id) => data.species.find((identfy) => identfy.id === id));
+  return species;
 }
 
 module.exports = getSpeciesByIds;
