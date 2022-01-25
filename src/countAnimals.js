c7661172d6aa2f56a0d5c08d99b49773149727b3
SheilaNS <shei.nsantos@gmail.com>
2022-01-25 18:52:37 -0300

    Arrumando o Lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  const [ specie ] = animal;
+  const [specie] = animal;
   const pop = data.species.find((nome) => nome.name === specie).popularity;
   return pop;
 }
