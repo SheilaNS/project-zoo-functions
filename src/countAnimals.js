93655871482835cceafc0ac17328ea5d46b6b0d7
SheilaNS <shei.nsantos@gmail.com>
2022-01-26 15:33:43 -0300

    Requisito 5 ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,10 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  const [specie] = animal;
-  const pop = data.species.find((nome) => nome.name === specie).popularity;
-  return pop;
+  if (animal === undefined) {
+    const allAnimals = {};
+    data.species.forEach((animals) => {
+      allAnimals[animals.name] = animals.residents.length;
+    });
+    return allAnimals;
+  }
+  const animalName = Object.values(animal);
+  const pop = data.species.find((nome) => nome.name === animalName[0]);
+  if (animalName.length === 1) {
+    return pop.residents.length;
+  }
+  if (animalName.length === 2) {
+    const popSex = pop.residents.filter((animalFilter) => animalFilter.sex === animalName[1]);
+    return popSex.length;
+  }
 }
 
 module.exports = countAnimals;
-console.log(countAnimals({ specie: 'penguins' }));
